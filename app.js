657f2549e7bd2ceecc03dcacb23633be19193971
Sebyeol23 <lixxil2323@gmail.com>
2023-12-18 01:24:39 +0900

    createUser 수정

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const db = require('./db/connection')
+const app = express();
+const port = 3000;
 
 const accountRouter = require('./routes/account');
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
--- controllers/account.js
+++ controllers/account.js
@@ -1,13 +1,13 @@
-const db = require('../db/connection');
+const pool = require('../db/connection');
 
 function createUser(req, res){
-    db.connect((error)=>{
+    pool.getConnection((error, db)=>{
         if(error){
-            db.end();
+            db.release();
             return res.status(500).json({error: error});
         }
-        db.query(`INSERT INTO user(id, password, name) VALUES('user2', '232', '게스트')`, (error)=>{
-            db.end();
+        db.query(`INSERT INTO user(id, password, name) VALUES(${JSON.stringify(req.body.id)}, ${JSON.stringify(req.body.pw)}, '게스트')`, (error)=>{
+            db.release();
             if(error) return res.status(400).json({error: error});
             return res.sendStatus(200);
         })
