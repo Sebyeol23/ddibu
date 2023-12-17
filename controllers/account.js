d4eed1cdbfa630e06c02f6ac75acee6e45316e05
Sebyeol23 <lixxil2323@gmail.com>
2023-12-17 20:01:50 +0900

    createUser 구현

--- controllers/account.js
+++ controllers/account.js
@@ -1,10 +1,17 @@
-async function createUser(req, res, next){
-    try{
-        const result = await "good";
-        res.send(result);
-    } catch(e){
-        next(e);
-    }
+const db = require('../db/connection');
+
+function createUser(req, res){
+    db.connect((error)=>{
+        if(error){
+            db.end();
+            return res.status(500).json({error: error});
+        }
+        db.query(`INSERT INTO user(id, password, name) VALUES('user2', '232', '게스트')`, (error)=>{
+            db.end();
+            if(error) return res.status(400).json({error: error});
+            return res.sendStatus(200);
+        })
+    })
 }
 
 module.exports = {
