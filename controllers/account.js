6ed980eba049b57d96b0f444c4c820efd48984f3
Sebyeol23 <lixxil2323@gmail.com>
2023-12-19 03:49:58 +0900

    문자열 관련 오류 수정

--- controllers/account.js
+++ controllers/account.js
@@ -7,9 +7,9 @@ function createUser(req, res){
         if(error){
             return res.status(500).json({error: error});
         }
-        bcrypt.hash(JSON.stringify(req.body.pw), 10, (error, pw)=>{
+        bcrypt.hash(req.body.pw, 10, (error, pw)=>{
             if(error) return res.status(400).json({error: error});
-            db.query(`INSERT INTO user(id, password, name) VALUES(${JSON.stringify(req.body.id)}, '${pw}', '게스트')`, (error)=>{
+            db.query(`INSERT INTO user(id, password, name) VALUES('${req.body.id}', '${pw}', '게스트')`, (error)=>{
                 db.release();
                 if(error) return res.status(400).json({error: error});
                 return res.sendStatus(200);
@@ -21,19 +21,18 @@ function createUser(req, res){
 function getToken(req, res){
     pool.getConnection((error, db)=>{
         if(error) return res.status(500).json({error: error});
-        db.query(`SELECT password FROM user WHERE id = ${JSON.stringify(req.body.id)}`, (error, results)=>{
+        db.query(`SELECT password FROM user WHERE id = '${req.body.id}'`, (error, results)=>{
             db.release();
             if(error) return res.status(400).json({error: error});
             if(!results.length) return res.status(404).json({error: "등록되지 않은 id 입니다."});
-            bcrypt.compare(JSON.stringify(req.body.pw), results[0].password, (error, same)=>{
+            bcrypt.compare(req.body.pw, results[0].password, (error, same)=>{
                 if(error) return res.status(500).json({error: error});
                 if(!same) return res.status(400).json({error: "비밀번호가 일치하지 않습니다."});
 
                 const key = 'key'; //환경변수로 바꿔줘야 함
-                const id = JSON.stringify(req.body.id);
                 const token = jwt.sign(
                     {
-                        userId: id
+                        userId: req.body.id
                     },
                     key,
                     {
