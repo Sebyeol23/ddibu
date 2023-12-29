66457dee5921f9ebf1403c3292ce4a5c1d4ccbc9
Sebyeol23 <lixxil2323@gmail.com>
2023-12-29 11:12:31 +0900

    getChat 구현

--- controllers/chat.js
+++ controllers/chat.js
@@ -8,14 +8,29 @@ function getChatRoom(req, res){
             if(error) return res.status(400).json({error: error});
             var chatRooms = [];
             results.forEach(element => {
-                chatRooms.push({chatRoomId: element.id, partnerId: req.decoded.userId == element.buyer ? element.seller : element.buyer});
+                chatRooms.push({roomId: element.id, partnerId: req.decoded.userId == element.buyer ? element.seller : element.buyer});
             });
-            console.log(chatRooms);
-            return res.sendStatus(200);
+            return res.status(200).json(chatRooms);
+        });
+    });
+}
+
+function getChat(req, res){
+    pool.getConnection((error, db)=>{
+        if(error) return res.status(500).json({error: error});
+        db.query(`SELECT message, date, status, sender FROM chat WHERE rid = '${req.body.roomId}' ORDER BY date DESC`, (error, results)=>{
+            db.release();
+            if(error) return res.status(400).json({error: error});
+            var chats = [];
+            results.forEach(element => {
+                chats.push({message: element.message, date: element.date, status: element.status, isSender: eq.decoded.userId == element.sender ? true : false});
+            });
+            return res.status(200).json(chats);
         });
     });
 }
 
 module.exports = {
-    getChatRoom
+    getChatRoom,
+    getChat
 }
--- routes/chat.js
+++ routes/chat.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const {auth} = require('../controllers/auth');
-const {getChatRoom} = require('../controllers/chat');
+const {getChatRoom, getChat} = require('../controllers/chat');
 
 router.route('/chat-room').get(auth, getChatRoom);
+router.route('/chat').get(auth, getChat);
 
 module.exports = router
