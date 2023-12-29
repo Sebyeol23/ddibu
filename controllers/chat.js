2fea53c9c1584c24eaa80a8596bc04aae8d69a54
Sebyeol23 <lixxil2323@gmail.com>
2023-12-29 09:34:17 +0900

    getChatRoom res 형식 수정

--- controllers/chat.js
+++ controllers/chat.js
@@ -3,10 +3,14 @@ const pool = require('../db/connection');
 function getChatRoom(req, res){
     pool.getConnection((error, db)=>{
         if(error) return res.status(500).json({error: error});
-        db.query(`SELECT chatRoom.id FROM chatRoom JOIN product ON chatRoom.pid = product.id WHERE chatRoom.buyer = '${req.decoded.userId}' OR product.seller = '${req.decoded.userId}'`, (error, results)=>{
+        db.query(`SELECT chatRoom.id, chatRoom.buyer, product.seller FROM chatRoom JOIN product ON chatRoom.pid = product.id WHERE chatRoom.buyer = '${req.decoded.userId}' OR product.seller = '${req.decoded.userId}'`, (error, results)=>{
             db.release();
             if(error) return res.status(400).json({error: error});
-            console.log(results);
+            var chatRooms = [];
+            results.forEach(element => {
+                chatRooms.push({chatRoomId: element.id, partnerId: req.decoded.userId == element.buyer ? element.seller : element.buyer});
+            });
+            console.log(chatRooms);
             return res.sendStatus(200);
         });
     });
