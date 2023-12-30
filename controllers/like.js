0b745d737817aa98b7a3bcc9fb67710bfd873224
Sebyeol23 <lixxil2323@gmail.com>
2023-12-30 23:27:43 +0900

    getLike 수정

--- controllers/like.js
+++ controllers/like.js
@@ -1,16 +1,33 @@
 const pool = require('../db/connection');
+const path = require('path');
+const fs = require('fs');
 
 function getLike(req, res){
+    // pool.getConnection((error, db)=>{
+    //     if(error) return res.status(500).json({error: error});
+    //     db.query(`SELECT pid FROM wishList WHERE uid = '${req.decoded.userId}'`, (error, results)=>{
+    //         db.release();
+    //         if(error) return res.status(400).json({error: error});
+    //         var likes = [];
+    //         results.forEach(element => {
+    //             likes.push({productId: element.pid});
+    //         });
+    //         return res.status(200).json(likes);
+    //     });
+    // });
     pool.getConnection((error, db)=>{
         if(error) return res.status(500).json({error: error});
-        db.query(`SELECT pid FROM wishList WHERE uid = '${req.decoded.userId}'`, (error, results)=>{
+        db.query(`SELECT product.id, product.title, product.price, product.date, product.body, product.status, product.seller, image.link FROM wishList JOIN product ON wishList.pid = product.id LEFT JOIN image ON product.id = image.pid WHERE uid = '${req.decoded.userId}' ${req.query.lastId ? `AND product.id < ${req.query.lastId} ` : ""}ORDER BY product.id DESC LIMIT ${req.query.limit}`, (error, results)=>{
             db.release();
             if(error) return res.status(400).json({error: error});
-            var likes = [];
+            var products = [];
             results.forEach(element => {
-                likes.push({productId: element.pid});
+                const imageExtension = element.link ? element.link.split('.').pop() : null;
+                const imagePath = element.link ? path.join(__dirname, '../public/images', element.link) : null;
+                const imageBuffer = element.link ? fs.readFileSync(imagePath) : null;
+                products.push({productId: element.id, title: element.title, body: element.body, price: element.price, date: element.date, location: element.location, status: element.status, sellerId: element.seller, image: element.link ? imageBuffer.toString('base64') : null, extension: element.link ? imageExtension : null});
             });
-            return res.status(200).json(likes);
+            return res.status(200).json(products);
         });
     });
 }
