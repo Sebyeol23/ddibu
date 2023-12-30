6bc90c028b968a9d8b946fe615d031a0349455e5
Sebyeol23 <lixxil2323@gmail.com>
2023-12-31 01:19:42 +0900

    getChatRoom 구현

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {auth} = require('../controllers/auth');
-const {getUser, createChatRoom, createLike, getProduct, getProductInfo} = require('../controllers/home');
+const {getUser, createChatRoom, createLike, getProduct, getProductInfo, getChatRoom} = require('../controllers/home');
 
 router.route('/user').get(auth, getUser);
-router.route('/chat-room').post(auth, createChatRoom);
+router.route('/chat-room').get(auth, getChatRoom).post(auth, createChatRoom);
 router.route('/like').post(auth, createLike);
 router.route('/product').get(getProduct);
 router.route('/product-info').get(getProductInfo);
