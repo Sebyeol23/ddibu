5c1557cf186742cab2363cb4c451ccf67f4f9fea
Sebyeol23 <lixxil2323@gmail.com>
2023-12-30 23:36:37 +0900

    주석 삭제

--- controllers/like.js
+++ controllers/like.js
@@ -3,18 +3,6 @@ const path = require('path');
 const fs = require('fs');
 
 function getLike(req, res){
-    // pool.getConnection((error, db)=>{
-    //     if(error) return res.status(500).json({error: error});
-    //     db.query(`SELECT pid FROM wishList WHERE uid = '${req.decoded.userId}'`, (error, results)=>{
-    //         db.release();
-    //         if(error) return res.status(400).json({error: error});
-    //         var likes = [];
-    //         results.forEach(element => {
-    //             likes.push({productId: element.pid});
-    //         });
-    //         return res.status(200).json(likes);
-    //     });
-    // });
     pool.getConnection((error, db)=>{
         if(error) return res.status(500).json({error: error});
         db.query(`SELECT product.id, product.title, product.price, product.date, product.body, product.status, product.seller, image.link FROM wishList JOIN product ON wishList.pid = product.id LEFT JOIN image ON product.id = image.pid WHERE uid = '${req.decoded.userId}' ${req.query.lastId ? `AND product.id < ${req.query.lastId} ` : ""}ORDER BY product.id DESC LIMIT ${req.query.limit}`, (error, results)=>{
