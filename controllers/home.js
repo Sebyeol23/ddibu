ca479fdfe6831253f1006e8da158fe9b93057833
Sebyeol23 <lixxil2323@gmail.com>
2023-12-31 08:45:10 +0900

    createChatRoom io 이벤트

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,6 @@
 const pool = require('../db/connection');
 const path = require('path');
 const fs = require('fs');
-const {io} = require('../app');
 
 function getUser(req, res){
     res.send(req.decoded);
@@ -45,9 +44,7 @@ function createChatRoom(req, res){
             db.query(`INSERT INTO chat(message, date, status, rid, sender) VALUES('${req.body.message}', '${req.body.date}', 0, ${results.insertId}, '${req.decoded.userId}')`, (error)=>{
                 db.release();
                 if(error) return res.status(400).json({error: error});
-                io.on('connection', (socket) => {
-                    io.emit('newChatRoom', results.insertId);
-                });
+                req.io.emit('newChatRoom', results.insertId);
                 return res.sendStatus(200);
             });
         });
