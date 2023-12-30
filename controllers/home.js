876f9c770b92ec0d4b19235e49691bda3e7113cc
Sebyeol23 <lixxil2323@gmail.com>
2023-12-31 07:28:36 +0900

    createChatRoom sql 오류 수정

--- controllers/home.js
+++ controllers/home.js
@@ -36,14 +36,10 @@ function createChatRoom(req, res){
         if(error){
             return res.status(500).json({error: error});
         }
-        db.query(`SELECT seller FROM product WHERE id = '${req.body.productId}'`, (error, results)=>{           
+        db.query(`SELECT seller FROM product WHERE id = '${req.body.productId}'`, (error)=>{           
             if(error){
                 db.release();
                 return res.status(400).json({error: error});
-            }
-            if(results[0].seller == req.decoded.userId){
-                db.release();
-                return res.status(400).json({error: "판매자와 구매자가 같습니다."});
             }
             db.query(`INSERT INTO chatRoom(buyer, pid, date) VALUES('${req.decoded.userId}', '${req.body.productId}', '${req.body.date}')`, (error)=>{
                 db.release();
