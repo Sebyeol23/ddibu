f6ae857e7839f6e47aa5aa9a944392e04a4705cc
Sebyeol23 <lixxil2323@gmail.com>
2023-12-31 11:02:07 +0900

    createChat 구현

--- routes/chat.js
+++ routes/chat.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const {auth} = require('../controllers/auth');
-const {getChatRoom, getChat} = require('../controllers/chat');
+const {getChatRoom, getChat, createChat} = require('../controllers/chat');
 
 router.route('/chat-room').get(auth, getChatRoom);
-router.route('/chat').get(auth, getChat);
+router.route('/chat').get(auth, getChat).post(auth, createChat);
 
 module.exports = router
