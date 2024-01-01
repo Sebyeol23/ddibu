fce0b76d1ebd704c320076b733af550acd7538c0
Sebyeol23 <lixxil2323@gmail.com>
2024-01-01 23:27:54 +0900

    auth require

--- routes/socket.js
+++ routes/socket.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const {auth} = require('../controllers/auth');
 const {createSocketInfo} = require('../controllers/socket');
 
 router.route('/socket').post(auth, createSocketInfo);
